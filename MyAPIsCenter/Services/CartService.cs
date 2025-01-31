42cf79e89498cd4287f1637b0b9ef454b2f0f7b0
amitjoshipune <amitjoshipune@gmail.com>
2025-01-31 11:16:20 +0530

    CartService - add to carts method updated for sync cache and _carts lists to be in sync and able persist cart items added.

--- MyAPIsCenter/Services/CartService.cs
+++ MyAPIsCenter/Services/CartService.cs
@@ -28,6 +28,62 @@ namespace YourNamespace.Services
 
         public void AddToCart(string userId, CartItem cartItem)
         {
+
+            if (!_cache.TryGetValue(CacheKey, out List<Cart> carts))
+            {
+                carts = _carts;
+                _cache.Set(CacheKey, carts);
+            }
+
+            if (carts == null || carts.Count == 0)
+            {
+                carts = new List<Cart>();
+                _cache.Set(CacheKey, carts);
+            }
+
+            var userCarts = carts.FirstOrDefault(c => c.UserId == userId);
+            if (userCarts == null)
+            {
+                var aNewUserCart = new Cart { UserId = userId };
+                aNewUserCart.Items.Add(cartItem);
+                carts.Add(aNewUserCart);
+            }
+            else
+            {
+                var existingItem = userCarts.Items.FirstOrDefault(i => i.BookId == cartItem.BookId);
+                if (existingItem != null)
+                {
+                    existingItem.Quantity += cartItem.Quantity;
+                }
+                else
+                {
+                    userCarts.Items.Add(cartItem);
+                }
+            }
+
+            //// Do this for in memory list of carts
+            var _userCarts = _carts.FirstOrDefault(c => c.UserId == userId);
+            if (_userCarts == null)
+            {
+                var aNewUserCart = new Cart { UserId = userId };
+                aNewUserCart.Items.Add(cartItem);
+                _carts.Add(aNewUserCart);
+            }
+            else
+            {
+                var existingItem = _userCarts.Items.FirstOrDefault(i => i.BookId == cartItem.BookId);
+                if (existingItem != null)
+                {
+                    existingItem.Quantity += cartItem.Quantity;
+                }
+                else
+                {
+                    _userCarts.Items.Add(cartItem);
+                }
+            }
+            _cache.Set(CacheKey, _carts);
+
+            /*
             var cart = GetCartByUserId(userId);
             var existingItem = cart.Items.FirstOrDefault(i => i.BookId == cartItem.BookId);
             if (existingItem != null)
@@ -39,6 +95,7 @@ namespace YourNamespace.Services
                 cart.Items.Add(cartItem);
             }
             _cache.Set(CacheKey, _carts);
+            */
         }
 
         public void RemoveFromCart(string userId, string bookId)
