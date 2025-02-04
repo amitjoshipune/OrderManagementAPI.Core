7a4d894fd4a0753905ba8ee84cce19f616375c94
amitjoshipune <amitjoshipune@gmail.com>
2025-02-04 23:14:37 +0530

    started working with mongodb

---
+++ ShoppingCartService/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace ShoppingCartService
+{
+    public class ShoppingCartItem
+    {
+        public string BookId { get; set; }
+        public int Quantity { get; set; }
+        public string BookTitle { get; set; }
+    }
+    public class ShoppingCart
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ShoppingCartId { get; set; }
+
+        public string UserId { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+    }
+}
